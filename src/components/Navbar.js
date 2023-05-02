a2af271618f2d10efd6a8c0deca7a522d8117f51
Aneel Grewal <aneel.grewal@hotmail.com>
2023-05-03 00:51:50 +0200

    Navbar component created

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,16 @@
 
 const Navbar = () => {
     return(
-        <div></div>
+        <div className="text-white flex justify-between items-center max-w-[1240px] mx-auto h-24 px-4">
+            <h1 className="w-full text-3xl font-bold text-[#00df9a]"> REACT.</h1>
+            <ul className="flex justify-between">
+                <li className="p-4">Home</li>
+                <li className="p-4">Company</li>
+                <li className="p-4">Resources</li>
+                <li className="p-4">About</li>
+                <li className="p-4">Contact</li>
+            </ul>
+        </div>
     )
 }
 
