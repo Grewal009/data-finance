d71684a1c95736d87a4febf3d93c18dbd5cbddbb
Aneel Grewal <aneel.grewal@hotmail.com>
2023-05-04 00:07:01 +0200

    Navbar component created (responsive)

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,52 @@
+import { useState } from "react";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 
 const Navbar = () => {
-    return(
-        <div className="text-white flex justify-between items-center max-w-[1240px] mx-auto h-24 px-4">
-            <h1 className="w-full text-3xl font-bold text-[#00df9a]"> REACT.</h1>
-            <ul className="flex justify-between">
-                <li className="p-4">Home</li>
-                <li className="p-4">Company</li>
-                <li className="p-4">Resources</li>
-                <li className="p-4">About</li>
-                <li className="p-4">Contact</li>
-            </ul>
+  const [nav, setNav] = useState(false);
+  const handleNav = () => {
+    setNav(!nav);
+  };
+
+  return (
+    <div className="text-white flex justify-between items-center max-w-[1240px] mx-auto h-24 px-4">
+      <h1 className="w-full text-3xl font-bold text-[#00df9a]"> REACT.</h1>
+      <ul className="hidden sm:block sm:flex">
+        <li className="p-4">Home</li>
+        <li className="p-4">Company</li>
+        <li className="p-4">Resources</li>
+        <li className="p-4">About</li>
+        <li className="p-4">Contact</li>
+      </ul>
+      <div onClick={handleNav} className=" sm:hidden">
+        {!nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+      </div>
+      <div className="sm:hidden">
+        <div
+          className={
+            !nav
+              ? "fixed left-0 top-20 w-full h-full border-r-gray-900 bg-[#000300]"
+              : "fixed left-[-100%]"
+          }
+        >
+          <ul className="pt-2 uppercase ">
+            <li className="p-2 border-b flex justify-center border-gray-600">
+              Home
+            </li>
+            <li className="p-2 border-b flex justify-center border-gray-600">
+              Company
+            </li>
+            <li className="p-2 border-b flex justify-center border-gray-600">
+              Resources
+            </li>
+            <li className="p-2 border-b flex justify-center border-gray-600">
+              About
+            </li>
+            <li className="p-2 flex justify-center">Contact</li>
+          </ul>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default Navbar;
